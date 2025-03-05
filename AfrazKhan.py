146b3237058fb30046797280cd4ddc1e509eef15
Afraz M Khan <59584416+AfrazMKhan@users.noreply.github.com>
2025-03-05 14:58:46 -0500

    Add files via upload

---
+++ AfrazKhan.py
@@ -0,0 +1,91 @@
+"""
+Afraz Muhammad Khan
+CS 100 2021 Fall Section 109
+HW 03, September, 2021
+"""
+
+1. #Square
+import turtle
+s = turtle.Screen()
+gT = turtle.Turtle()
+gT.color('green')
+ts = 100
+gT.forward(ts)
+gT.left(90)
+gT.forward(ts)
+gT.left(90)
+gT.forward(ts)
+gT.left(90)
+gT.forward(ts)
+
+# Triangle
+import turtle
+t_size = 100
+bT = turtle.Turtle()
+bT.color('blue')
+bT.forward(t_size)
+bT.left(120)
+bT.forward(t_size)
+bT.left(120)
+bT.forward(t_size)
+bT.left(120)
+
+# Regular Pentagon
+import turtle
+ts = 100
+rP = turtle.Turtle()
+rP.color('red')
+rP.forward(ts)
+rP.left(72)
+rP.forward(ts)
+rP.left(72)
+rP.forward(ts)
+rP.left(72)
+rP.forward(ts)
+rP.left(72)
+rP.forward(ts)
+rP.left(72)
+"""
+"""
+# Circle
+import turtle
+c1 = turtle.Turtle()
+c1.color('green')
+c1.up()
+c1.goto(0,-100)
+c1.down()
+c1.circle(200)
+c2 = turtle.Turtle()
+c2.color('blue')
+c2.up()
+c2.goto(0,-50)
+c2.down()
+c2.circle(150)
+c3 = turtle.Turtle()
+c3.color('pink')
+c3.up()
+c3.goto(0,0)
+c3.down()
+c3.circle(100)
+c4 = turtle.Turtle()
+c4.color('red')
+c4.up()
+c4.goto(0,50)
+c4.down()
+c4.circle(50)
+
+import math
+
+print("100 factorial =",math.factorial(100),"\n")
+
+print("The log (base 2) of 1,000,000 =",math.log(1000000,2),"\n")
+
+print("The greatest common divisor of 63 and 49 =",math.gcd(63, 49))
+
+
+
+
+
+
+
+
